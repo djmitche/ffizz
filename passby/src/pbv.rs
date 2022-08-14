31d51f425d88d6c3c50ea9db435e1252ecdc7e64
Dustin J. Mitchell <dustin@v.igoro.us>
2022-08-14 23:59:37 +0000

    remove PassByValue::take_val_from_arg, as it's unnecessary

--- passby/src/pbv.rs
+++ passby/src/pbv.rs
@@ -34,59 +34,6 @@ pub trait PassByValue: Sized {
         unsafe { arg.from_ctype() }
     }
 
-    /// Take a value from C as a pointer argument, replacing it with the given value.  This is used
-    /// to invalidate the C value as an additional assurance against subsequent use of the value.
-    ///
-    /// Most uses of this trait do not require invalidation to ensure correctness, so it is unusual
-    /// to use this method.
-    ///
-    /// # Safety
-    ///
-    /// - arg must not be NULL
-    /// - *arg must be a valid, properly aligned instance of the C type
-    ///
-    /// # Example
-    ///
-    /// Consider a `foo_file_t` that wraps a file descriptor.  The C API can avoid use of this
-    /// descriptor after close by invalidating the file descriptor when closing.
-    ///
-    /// Note that the Rust type used here must _not_ automatically close the file on drop!
-    ///
-    /// ```rust
-    /// # use ffizz_passby::PassByValue;
-    /// #[repr(C)]
-    /// struct foo_file_t { fd: i64 }
-    /// # struct File(i64);
-    /// # impl File {
-    /// #     fn close(self) { todo!() }
-    /// # }
-    /// # impl PassByValue for foo_file_t {
-    /// #     type RustType = File;
-    /// #     unsafe fn from_ctype(self) -> Self::RustType { todo!() }
-    /// #     fn as_ctype(arg: Self::RustType) -> Self { todo!() }
-    /// # }
-    /// /// Close a foo_file_t. The given file must not be NULL and must point to a valid, open
-    /// /// foo_file_t. The file cannot be used after this call.
-    /// #[no_mangle]
-    /// pub unsafe extern "C" fn foo_file_close(file: *mut foo_file_t) {
-    ///     // SAFETY:
-    ///     // - file is not NULL (see docstring)
-    ///     // - *file is a valid foo_file_t (see docstring)
-    ///     let file = unsafe {
-    ///         foo_file_t::take_val_from_arg(file, foo_file_t { fd: -1 })
-    ///     };
-    ///     file.close();
-    /// }
-    unsafe fn take_val_from_arg(arg: *mut Self, mut replacement: Self) -> Self::RustType {
-        // SAFETY:
-        //  - arg is valid (see docstring)
-        //  - replacement is valid and aligned (guaranteed by Rust)
-        unsafe { std::ptr::swap(arg, &mut replacement) };
-        // SAFETY:
-        //  - replacement (formerly *arg) is a valid CType (see docstring)
-        unsafe { PassByValue::val_from_arg(replacement) }
-    }
-
     /// Return a value to C
     ///
     /// # Safety
