92b0d5079fdca834549446b84ba0a2f539ce55d9
Dustin J. Mitchell <dustin@v.igoro.us>
2022-08-24 20:30:08 +0000

    parse HeaderItem out of token stream

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,53 +1,86 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Span, TokenStream as TokenStream2};
 use quote::quote;
-use syn::parse::Error;
+use syn::parse::{Error, Parse, ParseStream, Result};
 
+#[derive(Debug, PartialEq)]
 struct HeaderItem {
     order: usize,
     name: String,
     content: String,
 }
 
-#[proc_macro_attribute]
-pub fn function(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let parsed = {
-        let item = item.clone();
-        syn::parse_macro_input!(item as syn::ItemFn)
-    };
-    function2(TokenStream2::from(attr), TokenStream2::from(item), parsed).into()
-}
-
-fn function2(attr: TokenStream2, existing: TokenStream2, itemfn: syn::ItemFn) -> TokenStream2 {
-    // extract any docstring
-    let mut doc = vec![];
-    for attr in itemfn.attrs {
-        if let Ok(syn::Meta::NameValue(nv)) = attr.parse_meta() {
-            if nv.path.is_ident("doc") {
-                if let syn::Lit::Str(s) = nv.lit {
-                    doc.push(s.value());
+impl Parse for HeaderItem {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let item: syn::Item = input.parse()?;
+        match item {
+            syn::Item::Fn(itemfn) => {
+                // extract any docstring
+                let mut doc = vec![];
+                for attr in itemfn.attrs {
+                    if let Ok(syn::Meta::NameValue(nv)) = attr.parse_meta() {
+                        if nv.path.is_ident("doc") {
+                            if let syn::Lit::Str(s) = nv.lit {
+                                doc.push(s.value());
+                            }
+                        }
+                    }
                 }
+
+                // extract the function name
+                let name = itemfn.sig.ident.to_string();
+
+                if doc.len() == 0 {
+                    return Result::Err(Error::new(
+                        Span::call_site(),
+                        format!("{} does not have a docstring", name),
+                    ));
+                }
+
+                Ok(parse_docstring(name, doc))
             }
+            _ => Result::Err(Error::new_spanned(
+                item,
+                "cannot determine header content from this item",
+            )),
         }
     }
+}
 
-    // extract the function name
-    let name = itemfn.sig.ident.to_string();
+impl HeaderItem {
+    fn into_tokens(self) -> TokenStream2 {
+        let HeaderItem {
+            order,
+            name,
+            content,
+        } = self;
+        let item_name = syn::Ident::new(&format!("FFIZZ_HDR__{}", name), Span::call_site());
 
-    if doc.len() == 0 {
-        return with_error(existing, format!("{} does not have a docstring", name));
+        quote! {
+            #[::ffizz_header::linkme::distributed_slice(::ffizz_header::FFIZZ_HEADER_ITEMS)]
+            #[linkme(crate=::ffizz_header::linkme)]
+            static #item_name: ::ffizz_header::HeaderItem = ::ffizz_header::HeaderItem {
+                order: #order,
+                name: #name,
+                content: #content,
+            };
+        }
+        .into()
     }
+}
 
-    let header_item = match parse_docstring(name, doc) {
-        Ok(header_item) => header_item,
-        Err(msg) => return with_error(existing, msg),
+#[proc_macro_attribute]
+pub fn function(attr: TokenStream, mut item: TokenStream) -> TokenStream {
+    let parsed = {
+        let item = item.clone();
+        syn::parse_macro_input!(item as HeaderItem)
     };
-
-    with_header_item(existing, header_item)
+    item.extend(TokenStream::from(parsed.into_tokens()));
+    item
 }
 
 /// parse a docstring to extract C declarations and comments.
-fn parse_docstring(name: String, doc: Vec<String>) -> Result<HeaderItem, String> {
+fn parse_docstring(name: String, doc: Vec<String>) -> HeaderItem {
     // TODO: strip common leading whitespace from all lines, leading/trailing empty
     // comment lines
     let mut content = vec![];
@@ -68,42 +101,30 @@ fn parse_docstring(name: String, doc: Vec<String>) -> Result<HeaderItem, String>
         }
     }
 
-    Ok(HeaderItem {
+    HeaderItem {
         order: 100, // default
         name,
         content: itertools::join(content, "\n"),
-    })
-}
-
-/// Return a token stream containing the existing content as well as a compile_error!() invocation
-/// with the given message
-fn with_error<T: std::fmt::Display>(mut existing: TokenStream2, error: T) -> TokenStream2 {
-    let addition = Error::new(Span::call_site(), error).to_compile_error();
-    existing.extend(addition);
-    existing
-}
-
-/// Return a token stream containing the existing content as well as a declaration of the given
-/// header item
-fn with_header_item(mut existing: TokenStream2, header: HeaderItem) -> TokenStream2 {
-    let HeaderItem {
-        order,
-        name,
-        content,
-    } = header;
-    let item_name = syn::Ident::new(&format!("FFIZZ_HDR__{}", name), Span::call_site());
-    let addition: TokenStream2 = quote! {
-        #[::ffizz_header::linkme::distributed_slice(::ffizz_header::FFIZZ_HEADER_ITEMS)]
-        #[linkme(crate=::ffizz_header::linkme)]
-        static #item_name: ::ffizz_header::HeaderItem = ::ffizz_header::HeaderItem {
-            order: #order,
-            name: #name,
-            content: #content,
-        };
     }
-    .into();
-    existing.extend(addition);
-    existing
 }
 
-// TODO: test
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parsing_fn() {
+        let hi: HeaderItem = syn::parse_quote! {
+            /// A docstring
+            pub unsafe extern "C" fn add(x: u32, y: u32) -> u32 {}
+        };
+        assert_eq!(
+            hi,
+            HeaderItem {
+                order: 100,
+                name: "add".into(),
+                content: "// A docstring".into(),
+            }
+        );
+    }
+}
