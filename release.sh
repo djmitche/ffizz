3e712bea34a8e768d4d6b4d541dc04928e7a60e2
Dustin J. Mitchell <dustin@v.igoro.us>
2022-08-30 01:57:45 +0000

    add sleeps to release script

--- release.sh
+++ release.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-# ordered by dependencies
+# ordered by dependencies, with sleep's in between to allow crates.io's DB to
+# catch up
 cargo publish -p ffizz-passby
+sleep 10
 cargo publish -p ffizz-macros
+sleep 10
 cargo publish -p ffizz-header
+sleep 10
 cargo publish -p ffizz-string
